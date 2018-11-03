87f3924eff6ff599044a8b221ce105f862e2ed6d
ka-eun <41510487+ka-eun@users.noreply.github.com>
2018-11-03 19:01:29 +0900

    Add files via upload

---
+++ flipandsave.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+import numpy as np
+
+
+path = "C:\\flipping\\bottle\\"
+
+for i, fname in enumerate(os.listdir(path)):
+	imgsrc = path+fname
+	img = cv2.imread(imgsrc)
+	flip_img = img.copy()
+	flip_img = cv2.flip(img, 1)
+	#cv2.imshow("horizontal", flip_img)
+	cv2.imwrite('flipped'+str(i)+'.png', flip_img)
+
+

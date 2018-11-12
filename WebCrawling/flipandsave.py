a23e569b280c0f38614b86860471907553893061
ka-eun <41510487+ka-eun@users.noreply.github.com>
2018-11-13 00:36:45 +0900

    Update flipandsave.py

--- WebCrawling/flipandsave.py
+++ WebCrawling/flipandsave.py
@@ -3,15 +3,12 @@ import cv2
 import numpy as np
 
 
-path = "C:\\resizing\\"
+path = "C:\\test\\new1\\"
 
 for i, fname in enumerate(os.listdir(path)):
-	imgsrc = path+fname
-	img = cv2.imread(imgsrc)
-	resize_img = img.copy()
-	dim = (32, 32)
-	resize_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-	cv2.imshow("horizontal", resize_img)
-	cv2.imwrite('flipped'+str(i)+'.png', resize_img)
-
-
+   imgsrc = path+fname
+   img = cv2.imread(imgsrc)
+   flip_img = img.copy()
+   flip_img = cv2.flip(img, 1)
+   #cv2.imshow("Flipped", flip_img)
+   cv2.imwrite('flipped'+str(i)+'.png', flip_img)

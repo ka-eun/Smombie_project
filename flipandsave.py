1d07f81d6bfa4a1acac2ae43ea8e937fc7929a92
ka-eun <41510487+ka-eun@users.noreply.github.com>
2019-03-15 13:42:37 +0900

    Update flipandsave.py

--- flipandsave.py
+++ flipandsave.py
@@ -12,6 +12,6 @@ for i, fname in enumerate(os.listdir(path)):
 	flip_img = cv2.flip(img, 1)
 	#cv2.imshow("horizontal", flip_img)
 	result_img = cv2.resize(flip_img, (224, 224)) 
-	cv2.imwrite('flipped'+str(i)+'.png', result_img)
+	cv2.imwrite('result_img'+str(i)+'.png', result_img)
 
 
